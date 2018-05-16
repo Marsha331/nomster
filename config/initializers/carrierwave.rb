e3d7ea954710a97c7613eb205e7abd4d5aebf822
Marsha331 <marsha331@gmail.com>
2018-05-16 05:04:44 +0000

    Fixed Heroku and S3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,16 +1,18 @@
 # config/initializers/carrierwave.rb
 
-CarrierWave.configure do |config|                       # required                      #required
- if Rails.env.test?
+CarrierWave.configure do |config|                          # required                      #required
+  if Rails.env.test?
     config.storage = :file
     config.enable_processing = false
   else
-      config.storage = :fog/aws
-      config.fog_credentials = {
-         provider:              'AWS',                        # required
-         aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
-          aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
-        }
-        config.fog_directory  = ENV["AWS_BUCKET"]              # required
+    config.fog_provider = "fog/aws"
+    config.fog_credentials = {
+      provider:              'AWS',                        # required
+      aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
+      aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
+    }
+    config.fog_directory  = ENV["AWS_BUCKET"]              # required
+    config.fog_public = true
+    config.storage = :fog
   end
 end
